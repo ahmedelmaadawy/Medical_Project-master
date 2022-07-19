76b0a23c60b1c4f54d2d94345bbc0920039090ce
ahmedelmaadawy <ahmed.elmaadawy03@gmail.com>
2022-07-19 15:46:26 +0200

    before dict

--- lib/Models/person.dart
+++ lib/Models/person.dart
@@ -15,17 +15,4 @@ class Dictionary {
   String name;
   String translation;
   Dictionary({required this.name, required this.translation});
-
-  List<Dictionary> dictionaryList (){
-     List< Dictionary > dictionary = [] ;
-      dictionary.add(Dictionary(name: "WBC", translation: "كرات الدم البيضاء"));
-      dictionary.add(Dictionary(name: "HGB", translation: "الهيموغلوبين"));
-      dictionary.add(Dictionary(name: "MCV", translation: "الحجم الكريوي المتوسط"));
-      dictionary.add(Dictionary(name: "MCH", translation: "متوسط كرات الهيموجلوبين"));
-      dictionary.add(Dictionary(name: "MCHC", translation: "تركيز ھيموغلوبين الكرية"));
-      dictionary.add(Dictionary(name: "LYMPH", translation: "عدد اللمفاويات"));
-      dictionary.add(Dictionary(name: "MONO", translation: "الخلايا الوحيدة"));
-      return dictionary;
-
-  }
 }
